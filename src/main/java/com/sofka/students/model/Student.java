164d597c9348a59287c672eb95d9ad7b53f6bf8c
Nestor Quiroga Suárez <nqs48@icloud.com>
2022-08-26 17:42:42 -0500

    Add Validations to fields

--- src/main/java/com/sofka/students/model/Student.java
+++ src/main/java/com/sofka/students/model/Student.java
@@ -4,6 +4,7 @@ package com.sofka.students.model;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import reactor.util.annotation.NonNull;
 
 @Data
 @Document(collection= "Student")
@@ -11,16 +12,46 @@ public class Student {
 
     @Id
     private String id;
+
+    @NonNull
     private String name;
+
+    @NonNull
     private String surName;
+
+    @NonNull
     private String documentType;
+
+    @NonNull
     private String document;
+
+    @NonNull
     private String address;
+
+    @NonNull
     private String representative;
+
+    @NonNull
     private String phone;
+
+    @NonNull
     private Integer grade;
+
+    @NonNull
     private Integer state;
 
+    /**
+     *
+     * @param id id generate for mongodb
+     * @param name student name type String
+     * @param surName student surname type String
+     * @param documentType type student's document type String
+     * @param document student document type String
+     * @param address student address type String
+     * @param representative student representative type String
+     * @param phone student phone type String
+     * @param grade student grade type Integer
+     */
     public Student(String id, String name, String surName, String documentType, String document, String address, String representative, String phone, Integer grade) {
         this.id = id;
         this.name = name;
