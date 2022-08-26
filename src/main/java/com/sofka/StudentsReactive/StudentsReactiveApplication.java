98ae8fb722eafa18ec26e798f64e0dac8e4e47e9
Nestor Quiroga Suárez <nqs48@icloud.com>
2022-08-26 12:39:35 -0500

    Create Project and configure database <MongoDB>

---
+++ src/main/java/com/sofka/StudentsReactive/StudentsReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.StudentsReactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/StudentsReactive/StudentsReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.StudentsReactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
