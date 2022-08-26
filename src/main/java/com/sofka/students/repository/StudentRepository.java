22bff9f49b8f43790d1ed0adfd6c2d6ea622a9ae
Nestor Quiroga Suárez <nqs48@icloud.com>
2022-08-26 13:03:42 -0500

    Creating Package Repository and Interface <StudentRepository>

---
+++ src/main/java/com/sofka/students/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.sofka.students.repository;
+
+
+import com.sofka.students.model.Student;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends ReactiveMongoRepository<Student, String> {
+}
